59c300365ffdbdd5b4c1630c883dcfbb787456cf
Reka <seraphstorm@163.com>
2017-05-27 10:04:38 +0800

    删除PageBuilder分页构造器，增强内部类构造器

--- src/main/java/me/junbin/commons/page/Page.java
+++ src/main/java/me/junbin/commons/page/Page.java
@@ -9,12 +9,12 @@ import java.util.Objects;
  * @author : Zhong Junbin
  * @email : <a href="mailto:rekadowney@163.com">发送邮件</a>
  * @createDate : 2017/1/26 10:04
- * @description :
+ * @description : 分页封装模型
  */
 public class Page<T> implements Serializable {
 
     /**
-     * 当前查询实际拥有的数据量
+     * 当前查询实际拥有的数据量，会根据偏移量、页面数据量以及当前页内容数量进行微调
      */
     private final long totalElements;
 
@@ -73,7 +73,7 @@ public class Page<T> implements Serializable {
      * <pre>
      * 　　例如：当前{@link #reqPagingNavigationNum} 为 10，{@link #totalPages} 为 20，那么：
      * 假设当前为第 5 页，导航栏中将只有 1、2、3、4 共 4 条前置分页导航页码；
-     * 而若当前为第 15 页，导航栏中将只有 5、6、7、8、9、10、11、12、13、14 共 10 条前置分页导航页码；
+     * 而若当前为第 15 页，导航栏中将有 5、6、7、8、9、10、11、12、13、14 共 10 条前置分页导航页码；
      * </pre>
      */
     private final int[] previousNavigation;
@@ -82,7 +82,7 @@ public class Page<T> implements Serializable {
      * 在导航栏中，符合 {@link #reqPagingNavigationNum} 约束下，当前页的后面能够存在多少个导航页码。
      * <pre>
      * 　　例如：当前{@link #reqPagingNavigationNum} 为 10，{@link #totalPages} 为 20，那么：
-     * 假设当前为第 5 页，导航栏中将只有 6、7、8、9、10、11、12、13、14、15 共 10 条后置分页导航页码；
+     * 假设当前为第 5 页，导航栏中将有 6、7、8、9、10、11、12、13、14、15 共 10 条后置分页导航页码；
      * 而若当前为第 15 页，导航栏中将只有 16、17、18、19、20 共 5 条后置分页导航页码；
      * </pre>
      */
@@ -200,10 +200,19 @@ public class Page<T> implements Serializable {
         return content;
     }
 
+    public static <T> Builder<T> custom(List<T> content) {
+        return new Builder<>(content);
+    }
+
+    /**
+     * {@link #pageRequest} 分页处理优先级高于 {@link #reqPageOffset}、{@link #reqPageSize}
+     * 和 {@link #reqPagingNavigationNum}。
+     */
     public static class Builder<T> {
 
         private long totalElements;
         private int totalPages;
+        private PageRequest pageRequest;
         private int reqPageOffset;
         private int reqPageSize;
         private int reqPagingNavigationNum = PageRequest.DEFAULT_NAVIGATION_NUM;
@@ -217,70 +226,38 @@ public class Page<T> implements Serializable {
         private boolean isLastPage;
         private List<T> content;
 
-        /**
-         * 仅仅提供了页面数据，此时构造而成的 {@link Page} 将直接根据该页面数据生成。
-         * 即有分页请求：
-         * 　　页码偏移量：0；
-         * 　　页面数据量：{@link #content} 列表的长度；
-         * 　　总数据量：{@link #content} 列表的长度。
-         *
-         * @param content 页面数据
-         */
-        public Builder(final List<T> content) {
-            this(content.size(), content);
-        }
-
-        /**
-         * 提供了总数据量以及页面数据，此时构成的 {@link Page} 有如下规则：
-         * 　　页码偏移量：0；
-         * 　　页面数据量：{@link #content} 列表的长度；
-         * 　　总数据量：若 {@link #content} 列表的长度大于 {@link #totalElements}，
-         * 则使用列表长度作为总数据量，否则使用 {@link #totalElements} 作为总数据量。
-         *
-         * @param totalElements 总数据量
-         * @param content       页面数据
-         */
-        public Builder(final long totalElements, final List<T> content) {
-            this(new PageRequest(0, content.size()), totalElements, content);
-        }
-
-        /**
-         * @param pageRequest   分页请求参数
-         * @param totalElements 总数据量
-         * @param content       页面数据
-         */
-        public Builder(final PageRequest pageRequest, final long totalElements, final List<T> content) {
-            Objects.requireNonNull(pageRequest);
-            Objects.requireNonNull(content);
-            int contentSize = content.size();
-            this.reqPagingNavigationNum = pageRequest.getPagingNavigationNum();
-            this.reqPageOffset = pageRequest.getPageOffset();
-            this.reqPageSize = pageRequest.getPageSize();
-            this.curPageOffset = reqPageOffset;
-            this.curPageSize = contentSize;
-            int previousPagesTotalSize = reqPageOffset * reqPageSize; // 当前页之前的所有记录总数
-            this.totalElements = contentSize != 0 && previousPagesTotalSize + reqPageSize
-                    > totalElements ? previousPagesTotalSize + contentSize : totalElements;
+        public Builder(List<T> content) {
             this.content = content;
         }
 
-        public Builder(final int pageOffset, final int pageSize, final long totalElements, final List<T> content) {
-            this(pageOffset, pageSize, PageRequest.DEFAULT_NAVIGATION_NUM, totalElements, content);
-        }
-
-        public Builder(final int pageOffset, final int pageSize, final int pagingNavigationNum,
-                       final long totalElements, final List<T> content) {
-            Objects.requireNonNull(content);
-            int contentSize = content.size();
-            this.reqPagingNavigationNum = pagingNavigationNum;
+        public Builder<T> pageOffset(final int pageOffset) {
             this.reqPageOffset = pageOffset;
+            return this;
+        }
+
+        public Builder<T> pageSize(final int pageSize) {
             this.reqPageSize = pageSize;
-            this.curPageOffset = reqPageOffset;
-            this.curPageSize = contentSize;
-            int previousPagesTotalSize = reqPageOffset * reqPageSize;
-            this.totalElements = contentSize != 0 && previousPagesTotalSize + reqPageSize
-                    > totalElements ? previousPagesTotalSize + contentSize : totalElements;
-            this.content = content;
+            return this;
+        }
+
+        public Builder<T> pagingNavigationNum(final int pagingNavigationNum) {
+            this.reqPagingNavigationNum = pagingNavigationNum;
+            return this;
+        }
+
+        public Builder<T> pageRequest(final PageRequest pageRequest) {
+            this.pageRequest = Objects.requireNonNull(pageRequest);
+            return this;
+        }
+
+        public Builder<T> totalElements(final long totalElements) {
+            this.totalElements = totalElements;
+            return this;
+        }
+
+        public Builder<T> content(List<T> content) {
+            this.content = Objects.requireNonNull(content);
+            return this;
         }
 
         public Page<T> build() {
@@ -288,6 +265,8 @@ public class Page<T> implements Serializable {
         }
 
         public Page<T> create() {
+            normalize();
+
             this.totalPages = reqPageSize == 0 ? 1 : (int) Math.ceil((double) totalElements / (double) reqPageSize);
             this.hasPreviousPage = curPageOffset > 0;
             this.isFirstPage = !hasPreviousPage;
@@ -341,7 +320,50 @@ public class Page<T> implements Serializable {
                     previousNavigation, nextNavigation,
                     content);
         }
-    }
 
+
+        private void normalize() {
+            Objects.requireNonNull(content);
+            int contentSize = content.size();
+            if (totalElements == 0) {
+                totalElements = contentSize;
+            }
+            if (Objects.nonNull(pageRequest)) {
+                preBuild(pageRequest, totalElements, content);
+                return;
+            }
+            if (this.reqPageSize == 0) {
+                this.reqPageSize = contentSize;
+            }
+            preBuild(reqPageOffset, reqPageSize, reqPagingNavigationNum, totalElements, content);
+        }
+
+        private void preBuild(final PageRequest pageRequest, final long totalElements, final List<T> content) {
+            int contentSize = content.size();
+            this.reqPagingNavigationNum = pageRequest.getPagingNavigationNum();
+            this.reqPageOffset = pageRequest.getPageOffset();
+            this.reqPageSize = pageRequest.getPageSize();
+            this.curPageOffset = reqPageOffset;
+            this.curPageSize = contentSize;
+            int previousPagesTotalSize = reqPageOffset * reqPageSize; // 当前页之前的所有记录总数
+            this.totalElements = contentSize != 0 && previousPagesTotalSize + reqPageSize
+                    > totalElements ? previousPagesTotalSize + contentSize : totalElements;
+            this.content = content;
+        }
+
+        private void preBuild(final int pageOffset, final int pageSize, final int pagingNavigationNum,
+                              final long totalElements, final List<T> content) {
+            int contentSize = content.size();
+            this.reqPagingNavigationNum = pagingNavigationNum;
+            this.reqPageOffset = pageOffset;
+            this.reqPageSize = pageSize;
+            this.curPageOffset = reqPageOffset;
+            this.curPageSize = contentSize;
+            int previousPagesTotalSize = reqPageOffset * reqPageSize;
+            this.totalElements = contentSize != 0 && previousPagesTotalSize + reqPageSize
+                    > totalElements ? previousPagesTotalSize + contentSize : totalElements;
+            this.content = content;
+        }
+    }
 
 }
